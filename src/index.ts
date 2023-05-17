a6e4f3e181b11f8e6e0bc7836d306ca9252efff2
RamonBarth1 <ramongarcezbarth@gmail.com>
2023-05-17 20:27:26 -0300

    rodando-api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import express from "express"
+import express, {Request,Response} from "express"
+import { Growdever } from "./models/growdever";
 
 const app = express()
+
+app.get("/", (req:Request, res:Response) =>{
+    console.log("Entrou na rota");
+    const growdever1 = new Growdever("gr01", "Ramon", 12)
+    res.status(200).send({ok: true, data: growdever1.toJson()})
+});
 
 app.listen(3333, () => {
     console.log("Api bombando ");
