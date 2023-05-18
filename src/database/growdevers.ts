e6e9301b53566bdc334b1df74e3adce8a84b2ce2
RamonBarth1 <ramongarcezbarth@gmail.com>
2023-05-17 21:19:27 -0300

    criando-Json

---
+++ src/database/growdevers.ts
@@ -0,0 +1,6 @@
+import { Growdever } from "../models/growdever";
+
+export const growdevers = [
+    new Growdever("gr01","Maria",19),
+    new Growdever("gr02","José",14)
+]
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express, {Request,Response} from "express"
-import { Growdever } from "./models/growdever";
+import { growdevers } from "./database/growdevers";
 
-const app = express()
+const app = express();
 
-app.get("/", (req:Request, res:Response) =>{
-    console.log("Entrou na rota");
-    const growdever1 = new Growdever("gr01", "Ramon", 12)
-    res.status(200).send({ok: true, data: growdever1.toJson()})
-});
+// Rotas de growdevers => /growdever
+// GET https://localhost:3333/growdever
+
+app.get("/growdever", (req:Request, res: Response) => {
+try {
+    return res.status(200).send({
+        ok:true,
+        messaage: "Growdever listados com sucesso",
+        data: growdevers.map((growdever) => growdever.toJson()),
+    });
+    
+
+}catch (error:any){
+    return res.status(500).send({
+        ok:false,
+        message: error.toString(),
+    })
+}
+})
 
 app.listen(3333, () => {
     console.log("Api bombando ");
